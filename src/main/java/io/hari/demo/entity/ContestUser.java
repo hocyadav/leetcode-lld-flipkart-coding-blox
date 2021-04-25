d283cce6b3e8d2e0ab8107762d64b2b39848f12a
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-25 09:56:20 +0530

    entity

---
+++ src/main/java/io/hari/demo/entity/ContestUser.java
@@ -0,0 +1,12 @@
+package io.hari.demo.entity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author hayadav
+ * @create 4/24/2021
+ */
+public class ContestUser {
+    List<User> users = new LinkedList<>();
+}
